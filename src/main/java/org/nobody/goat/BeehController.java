ac1a7938b920fb8fb8eb2796292feb627fa9316b
Sylvain Hellegouarch <sylvain@atomist.com>
2017-09-11 22:09:07 +0200

    rest controller

---
+++ src/main/java/org/nobody/goat/BeehController.java
@@ -0,0 +1,16 @@
+package org.nobody.goat;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/api")
+public class BeehController {
+    @RequestMapping(value = "/v2", method = RequestMethod.GET)
+    public ResponseEntity<String> hello() {
+        return new ResponseEntity<String>("Hi there", HttpStatus.OK);
+    }
+}
